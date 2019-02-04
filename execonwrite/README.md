bd387b0a8246903fb642f038603b694f1bb09d98
em <eugen.mayer@kontextwork.de>
2019-02-04 13:48:41 +0100

    add switch for dev/stable variants

---
+++ execonwrite/README.md
@@ -0,0 +1,7 @@
+## tests
+
+    docker-compose build
+    # test current stable release
+    docker-compose up tiller
+    # test current dev release
+    docker-compose up tiller_dev
--- execonwrite/build/entrypoint.sh
+++ execonwrite/build/entrypoint.sh
@@ -1,16 +1,38 @@
 #!/usr/bin/env bash
+
+if [ "$use_develop" = "true" ]; then
+  echo "------------ -> using tiller from source tiller"
+  cd /tiller_source
+  git branch
+  gem install ./tiller*.gem
+else
+  echo "------------ -> using stable tiller"
+  gem install tiller
+fi
+
+# running tiller the first time, this should trigger exec, but due to --no-exec it should not
 echo "*********************************************************************************************** "
 echo "*****************************************  FIRST TIME ***************************************** "
 echo "*********************************************************************************************** "
 echo
 tiller -v -d --no-exec
 
+# since we run tiller the second time, we have no changes for sure. Still, we are running --no-exec still
+# so exec should not be triggered out of
 echo "*********************************************************************************************** "
 echo "***************************************** SECOND TIME ***************************************** "
 echo "*********************************************************************************************** "
 echo
 
 tiller -v -d --no-exec
+
+# this case should not trigger any exec also, since we run the same templates without changing any dynamic values
+echo "************************************************************************************************** "
+echo "***************************************** WITHOUT NO-EXEC **************************************** "
+echo "************************************************************************************************** "
+echo
+
+tiller -v -d
 
 shutdown ()
 {
